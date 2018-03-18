6b2822447532a2f30a4be84f722046274c1a48c9
davidlatuno <dslatuno@gmail.com>
2018-03-17 21:42:16 -0700

    Added picking logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,57 @@
-var charmander = $("#start").prepend("<img class='pokemon charmander' src='assets/images/500px-004Charmander.png' alt='charmander'>");
 
-var squirtle = $("#start").prepend("<img class='pokemon squirtle' src='assets/images/500px-007Squirtle.png' alt='squirtle'>");
+function go() {
 
-var bulbasaur = $("#start").prepend("<img class='pokemon bulbasaur' src='assets/images/500px-001Bulbasaur.png' alt='bulbasaur'>");
+    var charmander = $("#start").prepend("<img class='pokemon charmander' src='assets/images/500px-004Charmander.png' alt='charmander'>");
 
-var pikachu = $("#start").prepend("<img class='pokemon pikachu' src='assets/images/500px-025Pikachu.png' alt='pikachu'>");
+    var squirtle = $("#start").prepend("<img class='pokemon squirtle' src='assets/images/500px-007Squirtle.png' alt='squirtle'>");
+
+    var bulbasaur = $("#start").prepend("<img class='pokemon bulbasaur' src='assets/images/500px-001Bulbasaur.png' alt='bulbasaur'>");
+
+    var pikachu = $("#start").prepend("<img class='pokemon pikachu' src='assets/images/500px-025Pikachu.png' alt='pikachu'>");
+}
+
+function pikachu() {
+
+    var charmander = $("#enemy").prepend("<img class='pokemon charmander' src='assets/images/500px-004Charmander.png' alt='charmander'>");
+
+    var squirtle = $("#enemy").prepend("<img class='pokemon squirtle' src='assets/images/500px-007Squirtle.png' alt='squirtle'>");
+
+    var bulbasaur = $("#enemy").prepend("<img class='pokemon bulbasaur' src='assets/images/500px-001Bulbasaur.png' alt='bulbasaur'>");
+
+}
+
+function charmander() {
+
+    var squirtle = $("#enemy").prepend("<img class='pokemon squirtle' src='assets/images/500px-007Squirtle.png' alt='squirtle'>");
+
+    var bulbasaur = $("#enemy").prepend("<img class='pokemon bulbasaur' src='assets/images/500px-001Bulbasaur.png' alt='bulbasaur'>");
+
+    var pikachu = $("#enemy").prepend("<img class='pokemon pikachu' src='assets/images/500px-025Pikachu.png' alt='pikachu'>");
+
+
+}
+
+function squirtle() {
+
+    var charmander = $("#enemy").prepend("<img class='pokemon charmander' src='assets/images/500px-004Charmander.png' alt='charmander'>");
+
+    var bulbasaur = $("#enemy").prepend("<img class='pokemon bulbasaur' src='assets/images/500px-001Bulbasaur.png' alt='bulbasaur'>");
+
+    var pikachu = $("#enemy").prepend("<img class='pokemon pikachu' src='assets/images/500px-025Pikachu.png' alt='pikachu'>");
+
+
+}
+
+function bulbasaur() {
+
+    var charmander = $("#enemy").prepend("<img class='pokemon charmander' src='assets/images/500px-004Charmander.png' alt='charmander'>");
+
+    var squirtle = $("#enemy").prepend("<img class='pokemon squirtle' src='assets/images/500px-007Squirtle.png' alt='squirtle'>");
+
+    var pikachu = $("#enemy").prepend("<img class='pokemon pikachu' src='assets/images/500px-025Pikachu.png' alt='pikachu'>");
+
+
+}
 
 function name() {
 
@@ -15,16 +62,39 @@ function name() {
 
 }
 
+go();
 name();
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $(".pokemon").on("click", function () {
+    $("#start").on("click", ".pokemon", function () {
 
-        var pname = $(this).attr("data-pokename");
 
-        console.log(pname);
+        $("#start").empty();
 
+        $("#start").append(this);
+
+        var pick = $(this).attr("data-pokename");
+
+        if (pick === "pikachu") {
+
+            pikachu();
+        }
+
+        if (pick === "charmander") {
+
+            charmander();
+        }
+
+        if (pick === "bulbasaur") {
+
+            bulbasaur();
+        }
+
+        if (pick === "squirtle") {
+
+            squirtle();
+        }
     })
 
 
